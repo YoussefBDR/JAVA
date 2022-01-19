7551d94dae5c7d7552793c32dd448c2d42444f6e
YoussefBDR <96302786+YoussefBDR@users.noreply.github.com>
2022-01-19 18:02:10 +0100

    Delete TPS directory

--- TPS/TP8JPA/src/main/java/dao/IOrderDao.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import model.Order;
-
-public interface IOrderDao {
-    void save(Order o);
-}
--- TPS/TP8JPA/src/main/java/service/IOrderService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import dao.IOrderDao;
-import model.Order;
-
-public interface IOrderService {
-    void save(Order o);
-    public IOrderDao getDao();
-}
